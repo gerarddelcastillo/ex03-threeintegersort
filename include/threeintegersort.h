b863ab3cdc7be14a3a3e7cc8f2ee29c724db828d
gerardj.delcastillo@gmail.com <gerard_delcastillo1@my.vcccd.edu>
2018-12-11 09:39:58 -0800

    harder than fractions

--- include/threeintegersort.h
+++ include/threeintegersort.h
@@ -5,10 +5,14 @@
 #ifndef EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
 #define EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
 #include <stdio.h>
-namespace edu{
-    namespace vcccd {
-        namespace vc {
-            namespace csv13 {
+namespace edu
+{
+    namespace vcccd
+    {
+        namespace vc
+        {
+            namespace csv13
+            {
                 void sort(int numArr[]);
             }
         }
